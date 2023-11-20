fe9dfb60026041ffc987cca912aa0ae9eb7141bc
dankoda404@gmail.com <dankoda404@gmail.com>
2023-11-20 20:50:10 +0600

    babbel begin 20.11.23

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,16 @@
-'use strict';
+"use strict";
 
 var glo = 'academy';
 var age = 2;
+
+function func1() {
+    let name = "danil";
+    console.log(name);
+}
+
+function func2() {
+    console.log(name);
+}
+
+func1();
+func2();
--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 let glo ='academy';
 let age = 2;
+
+
+function func1 () {
+    let name = "danil";
+    console.log(name);
+}
+
+function func2 () {
+    console.log(name);
+}
+
+func1 ()
+func2 ()
