1f684be0491b510e726ead771353d465d17ed02b
dankoda404@gmail.com <dankoda404@gmail.com>
2023-11-23 18:14:10 +0600

    JS prototipical 23.11.23

--- dist/main.js
+++ dist/main.js
@@ -2,13 +2,26 @@
 
 var glo = 'academy';
 var age = 2;
+var name = "daniil";
 
 function func1() {
-    let name = "danil";
+    var name = "fghlhjtyhp";
+    surname = "Apple";
+    var surname = void 0;
+    if (true) {
+        console.log(name);
+    }
     console.log(name);
 }
+/* hoisting важно для интеревью. Вобщем можно написать прееменную и потом ей дать значение. let внутри блока имеет значение этого блока*/
 
 function func2() {
+    var name;
+    surname = "Dewa";
+    var surname = void 0;
+    if (true) {
+        console.log(name);
+    }
     console.log(name);
 }
 
--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 let glo ='academy';
 let age = 2;
+var name = "daniil";
 
 
 function func1 () {
-    let name = "danil";
+    let name = "fghlhjtyhp";
+    surname = "Apple";
+    let surname;
+    if (true) {
+        console.log(name);
+    } 
     console.log(name);
 }
+/* hoisting важно для интеревью. Вобщем можно написать прееменную и потом ей дать значение. let внутри блока имеет значение этого блока*/
 
 function func2 () {
+    var name;
+    surname = "Dewa";
+    let surname;
+    if (true) {
+        console.log(name);
+    } 
     console.log(name);
 }
 
