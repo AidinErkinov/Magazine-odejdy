5009a8e782bbd20429a6d140a6f5816ca1b201d8
dankoda404@gmail.com <dankoda404@gmail.com>
2023-11-27 20:54:09 +0600

    Webpack 27.11.23

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import {Program} from './program.js'; 
+var program = new Program (); 
+program.execute();
+program.execute();
+program.execute();
+program.execute();
+program.execute();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function func1 () {
+    surname = "Apple";
+    let surname, name;
+    if (true) {
+        console.log(name);
+    } 
+    console.log(name);
+}
+/* hoisting важно для интеревью. Вобщем можно дать значение, а потом через let и var дать переменную внутри блока имеет значение этого блока
+
+function func2 () {
+    var name;
+    surname = "Dewa";
+    let surname;
+    if (true) {
+        console.log(name);
+    } 
+    console.log(name);
+}
+
+
+func1 ()
+func2 ()
+ */ 
---
+++ src/program.js
@@ -0,0 +1,10 @@
+export class Program {
+    constructor () {
+        console.log("Program initialized");
+    }
+    execute () {
+        console.log("program executed");
+    }
+}
+
+/* */
